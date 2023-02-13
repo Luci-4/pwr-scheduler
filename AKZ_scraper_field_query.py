bf07f0e6135edd41d4574c8b980654bf6ba23fe1
wojci <wojciech.kurdybacha@gmail.com>
2023-02-13 11:14:52 +0100

    change field query to be case insensitive

--- AKZ_scraper_field_query.py
+++ AKZ_scraper_field_query.py
@@ -7,12 +7,12 @@ class AKZFieldQuery:
 
     def matches(self, name: str, teacher: str):
         if self.name and self.teacher:
-            return self.name in name and self.teacher in teacher
+            return self.name.lower() in name.lower() and self.teacher.lower() in teacher.lower()
 
-        if self.name and self.name in name:
+        if self.name and self.name.lower() in name.lower():
             return True
 
-        if self.teacher and self.teacher in teacher:
+        if self.teacher and self.teacher.lower() in teacher.lower():
             return True
 
         return False 
